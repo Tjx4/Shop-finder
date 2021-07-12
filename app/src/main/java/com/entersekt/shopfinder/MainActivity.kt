b5ca68c5777c355ae42acd05ce5aca41bcad28c5
Tshepo <rocboyt@gmail.com>
2021-07-12 08:05:03 +0200

    init commit

---
+++ app/src/main/java/com/entersekt/shopfinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.entersekt.shopfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
